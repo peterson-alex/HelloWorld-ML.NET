5d4d839044cca0254781838e1742ca9bfc3cb070
Alex Peterson <petersonalex@seattleu.edu>
2021-05-14 16:19:21 -0700

    Add test for Model Input default not null.

--- UnitTests/ModelInputTests.cs
+++ UnitTests/ModelInputTests.cs
@@ -3,10 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HelloWorld.MLModel;
+using NUnit.Framework;
 
 namespace UnitTests
 {
-    class ModelInputTests
+    public class ModelInputTests
     {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void ModelInput_Default_Not_Null()
+        {
+            // Act
+            var item = new ModelInput();
+
+            // Assert
+            Assert.NotNull(item);
+        }
     }
 }
