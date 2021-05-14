e177969bed7cf0d3340490ce74f3641cddb9f2ce
Alex Peterson <petersonalex@seattleu.edu>
2021-05-14 16:38:46 -0700

    Add ConsumeModelTests.cs to Unit Test project.

---
+++ UnitTests/ConsumeModelTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using HelloWorld.MLModel;
+
+namespace UnitTests
+{
+    public class ConsumeModelTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+    }
+}
